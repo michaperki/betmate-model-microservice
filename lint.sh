e73ef134e07a028984626e17ae95b0295d486928
Jack Keane <john.f.keane.22@dartmouth.edu>
2021-05-28 19:31:46 -0600

    fix: update requirements

--- lint.sh
+++ lint.sh
@@ -1,3 +1,3 @@
 source venv/bin/activate
-flake8 ./tests --count --select=E9,F63,F7,F82 --show-source --statistics
-flake8 ./tests --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics --ignore=F401
+flake8 ./src --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 ./src --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics --ignore=F401
